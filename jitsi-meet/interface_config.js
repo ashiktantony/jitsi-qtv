0fb92c5acc481bb7ac26cd15a4f93e7c92282be8
ashiktantony <57289185+ashiktantony@users.noreply.github.com>
2021-04-26 11:15:19 +0530

    Update interface_config.js

--- jitsi-meet/interface_config.js
+++ jitsi-meet/interface_config.js
@@ -130,7 +130,7 @@ var interfaceConfig = {
      * @type {number}
      */
 //     MAXIMUM_ZOOMING_COEFFICIENT: 1.3,
-    MAXIMUM_ZOOMING_COEFFICIENT: 1.3, //changed
+    MAXIMUM_ZOOMING_COEFFICIENT: 1.4, //changed
 
     /**
      * Whether the mobile app Jitsi Meet is to be promoted to participants
