1ef1518f71434e746dae8f44c38a9e012ecb1cc9
ashiktantony <57289185+ashiktantony@users.noreply.github.com>
2021-04-26 13:45:16 +0530

    DISABLECAM_REMOTE_PARTICIPANT

--- jitsi-meet/react/features/base/participants/middleware.js
+++ jitsi-meet/react/features/base/participants/middleware.js
@@ -20,6 +20,7 @@ import {
     KICK_PARTICIPANT,
     MUTE_REMOTE_PARTICIPANT,
 	UNMUTE_REMOTE_PARTICIPANT,
+    DISABLECAM_REMOTE_PARTICIPANT,
     PARTICIPANT_DISPLAY_NAME_CHANGED,
     PARTICIPANT_JOINED,
     PARTICIPANT_LEFT,
@@ -121,6 +122,12 @@ MiddlewareRegistry.register(store => next => action => {
     case UNMUTE_REMOTE_PARTICIPANT: {
         const { conference } = store.getState()['features/base/conference'];
         conference.unMuteParticipant(action.id, action.mediaType);
+        break;
+    }
+            
+    case DISABLECAM_REMOTE_PARTICIPANT: {
+        const { conference } = store.getState()['features/base/conference'];
+        conference.disableCamParticipant(action.id);
         break;
     }
     // TODO Remove this middleware when the local display name update flow is
