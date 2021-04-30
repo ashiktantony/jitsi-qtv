9c79c0ee68faaf44639cdd8163da351cc92318c7
ashiktantony <57289185+ashiktantony@users.noreply.github.com>
2021-04-30 23:32:29 +0530

    onMuteVideo

--- lib-jitsi-meet/modules/xmpp/ChatRoom.js
+++ lib-jitsi-meet/modules/xmpp/ChatRoom.js
@@ -1788,7 +1788,11 @@ export default class ChatRoom extends Listenable {
 
         if (mute.length && mute.text() === 'true') {
             this.eventEmitter.emit(XMPPEvents.VIDEO_MUTED_BY_FOCUS, mute.attr('actor'));
-        } else {
+        } 
+        else if (mute.length && mute.text() === 'false') { // added unmute condition
+            this.eventEmitter.emit(XMPPEvents.VIDEO_UNMUTED_BY_FOCUS, mute.attr('actor'));
+        }
+        else {
             // XXX Why do we support anything but muting? Why do we encode the
             // value in the text of the element? Why do we use a separate XML
             // namespace?
